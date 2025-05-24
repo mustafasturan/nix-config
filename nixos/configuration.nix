de0a2f18a806f516e5cdfaffbc17739499b93c4c
Mustafa Sencer Turan <mustafasturan@gmail.com>
2025-05-24 20:47:21 +0300

    Update configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -176,6 +176,7 @@
       # Be sure to change it (using passwd) after rebooting!
       isNormalUser = true;
       description = "Mustafa Turan";
+      shell = pkgs.zsh;
       openssh.authorizedKeys.keys = [
         # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
       ];
