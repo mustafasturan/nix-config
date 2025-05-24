bf173dc6baebd90a225b67a144f4c1deb1eba89f
Mustafa Sencer Turan <mustafasturan@gmail.com>
2025-05-24 20:06:12 +0300

    Update configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -137,7 +137,7 @@
     enable = true;                                                         
     settings = {                                                           
       default_session = {                                                  
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd sway --unsupported-gpu";                                           
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd sway";                                           
       };                                                                   
     };                                                                     
   };
