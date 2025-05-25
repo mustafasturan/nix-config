7ee51b0e88a821670d1c14049f007436b62dff41
Mustafa Sencer Turan <mustafasturan@gmail.com>
2025-05-25 17:05:18 +0300

    Update vscode.nix

--- home/mustafasturan/programs/vscode.nix
+++ home/mustafasturan/programs/vscode.nix
@@ -1,11 +1,10 @@
 { pkgs, ... }: {
   programs.vscode = {
     enable = true;
-    extensions = with pkgs.vscode-extensions; [
+    profiles.default.extensions = with pkgs.vscode-extensions; [
       ms-dotnettools.csharp
       esbenp.prettier-vscode
       dbaeumer.vscode-eslint
-      ms-vscode.vscode-typescript-next
     ];
   };
 }
