e8907c68e10fd6027b6bf2730fd35b2c5beefd4a
Mustafa Sencer Turan <mustafasturan@gmail.com>
2025-05-25 15:26:49 +0300

    Update home.nix

--- home/mustafasturan/home.nix
+++ home/mustafasturan/home.nix
@@ -1,9 +1,11 @@
-{ config, pkgs, ... }: {
-  home.username = "mustafasturan";
-  home.homeDirectory = "/home/mustafasturan";
-  programs.home-manager.enable = true;
-
-  imports = [
+{
+  inputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  imports = [  
     ./programs/docker.nix
     ./programs/dotnet.nix
     ./programs/git.nix
@@ -16,4 +18,36 @@
     ./programs/vscode.nix
     ./programs/zsh.nix
   ];
+
+  nixpkgs = {
+    overlays = [
+      # If you want to use overlays exported from other flakes:
+      # neovim-nightly-overlay.overlays.default
+
+      # Or define it inline, for example:
+      # (final: prev: {
+      #   hi = final.hello.overrideAttrs (oldAttrs: {
+      #     patches = [ ./change-hello-to-hi.patch ];
+      #   });
+      # })
+    ];
+    config = {
+      allowUnfree = true;
+      # Workaround for https://github.com/nix-community/home-manager/issues/2942
+      allowUnfreePredicate = _: true;
+    };
+  };
+
+  home = {
+    username = "mustafasturan";
+    homeDirectory = "/home/mustafasturan";
+  };
+
+  # Enable home-manager and git
+  programs.home-manager.enable = true;
+  programs.git.enable = true;
+
+  # Nicely reload system units when changing configs
+  systemd.user.startServices = "sd-switch";
+
 }
