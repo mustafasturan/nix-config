3afd195112b34c3c9762eace36fed33382190968
Mustafa Turan <mustafasturan@gmail.com>
2025-06-02 22:13:11 +0300

    fix

--- modules/docker.nix
+++ modules/docker.nix
@@ -2,25 +2,13 @@
   config,
   pkgs,
   lib,
-  userSettings,
-  storageDriver ? null,
   ...
 }:
-assert lib.asserts.assertOneOf "storageDriver" storageDriver [
-  null
-  "aufs"
-  "btrfs"
-  "devicemapper"
-  "overlay"
-  "overlay2"
-  "zfs"
-];
 
 {
   virtualisation.docker = {
     enable = true;
     enableOnBoot = true;
-    storageDriver = storageDriver;
     autoPrune.enable = true;
   };
   users.users.mustafasturan.extraGroups = [ "docker" ];
